f79df22737c820b5e17ae39e83ff6f72ee338840
NITESCU Cristian <az02289@frla000112.home>
2018-01-27 11:00:06 +0100

    Moved donkeyadmin to new repo

--- donkeyadmin/app/src/api/models.api.js
+++
@@ -1,7 +0,0 @@
-const request = require('superagent');
-
-export const getModels = async (baseUrl) => {
-    const resp = await request.get(`${baseUrl}/models`);
-    return (resp || {}).body || [];
-}
-
--- donkeyadmin/app/src/api/tubes.api.js
+++
@@ -1,7 +0,0 @@
-const request = require('superagent');
-
-export const getTubes = async (baseUrl) => {
-    const resp = await request.get(`${baseUrl}/tubes`);
-    return (resp || {}).body || [];
-}
-
