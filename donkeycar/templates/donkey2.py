357571b37cda7e4515e62798d72f873983c09de0
btrinite <benoit.trinite@gmail.com>
2018-05-21 17:10:42 +0200

    NeoPixel Led

--- donkeycar/templates/donkey2.py
+++ donkeycar/templates/donkey2.py
@@ -33,6 +33,33 @@ logger = logging.getLogger('donkey.manage')
 
 from docopt import docopt
 
+import time
+from neopixel import *
+
+# LED strip configuration:
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 64     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
+colorWipe(strip, Color(0,0,0), 10)
+colorWipe(strip, Color(255,0,0), 10)
+
+
 import donkeycar as dk
 
 # import parts
@@ -46,6 +73,7 @@ from donkeycar.parts.emergency import EmergencyController
 from donkeycar.parts.emergency import EmergencyController
 from donkeycar.parts.throttle_in_line import ThrottleInLine
 from sys import platform
+
 
 def drive(cfg, model_path=None, use_joystick=False, use_tx=False):
     '''
