9f2c43c1e85393cfc683a4ab9c988c8d3eeea74c
btrinite <benoit.trinite@gmail.com>
2018-12-11 10:21:34 +0100

    bug fix

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -112,7 +112,7 @@ class KerasCategorical1(KerasPilot):
             speedometer = 0.5
         img_arr = img_arr.reshape((1,) + img_arr.shape)
         speedometer_arr = np.array([speedometer]).reshape(1,1)
-        with dk.perfom.MeasureDuration('Model-Predict') as m:
+        with dk.perfmon.MeasureDuration('Model-Predict') as m:
             angle_binned, throttle, fullspeed_binned, brake_binned = self.model.predict([img_arr, speedometer_arr])
         #print('throttle', throttle)
         #angle_certainty = max(angle_binned[0])
